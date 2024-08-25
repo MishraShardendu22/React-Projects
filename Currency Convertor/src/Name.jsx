ae95aced460f3a4bf4fa0b6ab87eec844e189343
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-25 13:17:55 +0530

    Currency Convertor using Exchange Rate API !!!!

--- Currency Convertor/src/Name.jsx
+++ Currency Convertor/src/Name.jsx
@@ -4,8 +4,8 @@ let year = y.getFullYear();
 export default function Name() {
   return (
     <div >
-        <h1 className="my-10 text-center font-extrabold text-5xl rounded-lg shadow-lg text-red-200 hover:scale-110 transition duration-300 ease-in-out ">Made With ❤️ By Shardendu Mishra </h1>
-        <h1 className="my-10 text-center font-extrabold text-5xl rounded-lg shadow-lg text-red-200 hover:scale-110 transition duration-300 ease-in-out ">Made in : {year} ©️ of ShardenduMishra Create`s</h1>
+        <h1 className="my-10 text-center font-extrabold text-5xl rounded-lg text-red-200 hover:scale-110 transition duration-300 ease-in-out ">Made With ❤️ By Shardendu Mishra </h1>
+        <h1 className="my-10 text-center font-extrabold text-5xl rounded-lg text-red-200 hover:scale-110 transition duration-300 ease-in-out ">Made in : {year} ©️ of ShardenduMishra Create`s</h1>
     </div>
   )
 }
