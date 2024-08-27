1dc5cf3a693f8dd6c600c7f07c9b47cacc902f31
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-28 01:34:27 +0530

    To-Do List Using React !!!!

--- To Do List/src/App.jsx
+++ To Do List/src/App.jsx
@@ -41,23 +41,23 @@ function App() {
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4"> 
-                        <TodoForm />
-                    </div>
-                    <div className="flex flex-wrap gap-y-3">
-                        {todos.map((todo) => (
-                          <div key={todo.id}className='w-full'>
-                            <TodoItem todo={todo} />
-                          </div>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                  <Name />
-                </div>
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+            <div className="mb-4"> 
+              <TodoForm />
             </div>
+            <div className="flex flex-wrap gap-y-3">
+              {todos.map((todo) => (
+                <div key={todo.id}className='w-full'>
+                  <TodoItem todo={todo} />
+                </div>
+              ))}
+            </div>
+        </div>
+        <div>
+          <Name />
+        </div>
+      </div>
     </TodoProvider>
   )
 }
