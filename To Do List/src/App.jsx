f75af5edaee38c1a7e4f295135ee2473209e1c57
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-28 01:35:56 +0530

    To-Do List Using React !!!!

--- To Do List/src/App.jsx
+++ To Do List/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your ToDo's</h1>
             <div className="mb-4"> 
               <TodoForm />
             </div>
