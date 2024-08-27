fd6287daad9f84f744fa2c1c8241e350854cab63
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-28 01:26:57 +0530

    To-Do List Using React !!!!

---
+++ To Do List/src/components/index.js
@@ -0,0 +1,4 @@
+import TodoForm from "./ToDoForm";
+import TodoItem from "./ToDoItem";
+
+export {TodoForm, TodoItem};
---
+++ To Do List/src/contexts/TodoContext.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import {createContext, useContext} from "react"
+
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo Message",
+            completed: false
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
