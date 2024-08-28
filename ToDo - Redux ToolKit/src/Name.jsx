f1a379ffb13a25dc510bf170bb9cebc7b7bfe066
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-29 03:03:17 +0530

    To Do List Using React Redux Toolkit !!!!

--- ToDo - Redux ToolKit/src/Name.jsx
+++ ToDo - Redux ToolKit/src/Name.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <p className="text-2xl my-4 border-solid border-black font-bold rounded-md text-violet-600 hover:scale-110 font-extrabold text-4xl transition-transform duration-300 ease-in-out">
-        This Project uses context to make a To-Do list !!
+        This Project uses Redux-Toolkit to make a To-Do list !!
       </p>
       <p className="text-2xl my-4 border-solid border-black font-bold rounded-md text-violet-600 hover:scale-110 font-extrabold text-4xl transition-transform duration-300 ease-in-out">
         Made With ❤️ By Shardendu Mishra
